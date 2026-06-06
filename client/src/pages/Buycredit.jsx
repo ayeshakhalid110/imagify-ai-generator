94ef64773b0bdd9823e566a3022437f025436c05
Ayesha Khalid <ayeshaumer0190@gmail.com>
2026-06-07 01:57:06 +0500

    Ayesha Khalid - Added motion to the header

--- client/src/pages/Buycredit.jsx
+++ client/src/pages/Buycredit.jsx
@@ -8,7 +8,7 @@ const Buycredit = () => {
      const {user} = useContext(AppContext)
 
  return (
-  <motion.div 
+  <motion.div
   initial={{ opacity: 0.2, y: 100 }}
   transition={{ duration: 1 }}
   whileInView={{ opacity: 1, y: 0 }}
